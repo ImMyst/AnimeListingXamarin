5de311ea7400c59016315c5286c6895ef476a7b3
ImMyst/Titouan.G <titouan.galvani@laposte.net>
2019-03-13 09:47:41 +0100

    Initial commit :tada:

---
+++ README.md
@@ -0,0 +1,1 @@
+"# AnimeListingXamarin" 
---
+++ XamarinProject/XamarinProject/NewAnimePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinProject
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewAnimePage : ContentPage
+	{
+		public NewAnimePage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void saveAnimeEntry(object sender, EventArgs e)
+        {
+            DisplayAlert("Bien joué !", "L'anime a bien été enregistré", "Fermer");
+        }
+    }
+}
